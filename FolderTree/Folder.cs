2548010d41c417182c8659860183383fbfefaa95
Alexander Fedoniuk <35227010+whodeen@users.noreply.github.com>
2019-10-29 21:13:04 +0200

    Created basic class hierarchy

---
+++ FolderTree/Folder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystem
+{
+    class Folder : FileSystemEssence
+    {
+        public DateTime DateCreated { get; set; }
+        private List<Folder> childrens;
+        private List<File> files;
+
+        public List<Folder> GetChildrens()
+        {
+            return this.childrens;
+        }
+
+        public void SetChildren(Folder folder)
+        {
+            this.childrens.Add(folder);
+        }
+
+        public List<File> GetFiles()
+        {
+            return this.files;
+        }
+
+        public void SetFile(File file)
+        {
+            this.files.Add(file);
+        }
+
+        public Folder(string path)
+        {
+
+        }
+        
+    }
+}
